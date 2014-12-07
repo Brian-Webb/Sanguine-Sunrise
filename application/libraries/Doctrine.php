7af356f3a6e618d788501cdc17c7332bd58f47e6
Brian-Webb <webbsdomain@gmail.com>
2014-12-07 07:39:54 -0700

    Updating doctrine library to include 'driverOptions' to handle UTF8 charset in queries

--- application/libraries/Doctrine.php
+++ application/libraries/Doctrine.php
@@ -1,71 +1,62 @@
 <?php
 
+use Doctrine\Common\ClassLoader,
+    Doctrine\ORM\Tools\Setup,
+    Doctrine\ORM\Query\ResultSetMapping,
+    Doctrine\ORM\EntityManager;
+
+/**
+ * Doctrine bootstrap library for CodeIgniter
+ *
+ * @author	Joseph Wynn <joseph@wildlyinaccurate.com>
+ * @link	http://wildlyinaccurate.com/integrating-doctrine-2-with-codeigniter-2
+ */
 class Doctrine
 {
-    // the Doctrine entity manager
-    public $em = null;
+    public $em;
+    public $rsm;
 
     public function __construct()
     {
-        // include our CodeIgniter application's database configuration
-        require_once APPPATH.'config/database.php';
-        
-        // include Doctrine's fancy ClassLoader class
-        require_once APPPATH.'libraries/Doctrine/Common/ClassLoader.php';
+        require_once __DIR__.'/Doctrine/ORM/Tools/Setup.php';
+        Setup::registerAutoloadDirectory(__DIR__);
 
-        // load the Doctrine classes
-        $doctrineClassLoader = new \Doctrine\Common\ClassLoader('Doctrine', APPPATH.'libraries');
-        $doctrineClassLoader->register();
-        
-        // load Symfony2 helpers
-        // Don't be alarmed, this is necessary for YAML mapping files
-        $symfonyClassLoader = new \Doctrine\Common\ClassLoader('Symfony', APPPATH.'libraries/Doctrine');
-        $symfonyClassLoader->register();
+        // Load the database configuration from CodeIgniter
+        require APPPATH.'config/database.php';
 
-        // load the entities
-        $entityClassLoader = new \Doctrine\Common\ClassLoader('Entities', APPPATH.'models');
-        $entityClassLoader->register();
-
-        // load the proxy entities
-        $proxyClassLoader = new \Doctrine\Common\ClassLoader('Proxies', APPPATH.'models');
-        $proxyClassLoader->register();
-
-        // set up the configuration 
-        $config = new \Doctrine\ORM\Configuration;
-    
-        if(ENVIRONMENT == 'development')
-            // set up simple array caching for development mode
-            $cache = new \Doctrine\Common\Cache\ArrayCache;
-        else
-            // set up caching with APC for production mode
-            $cache = new \Doctrine\Common\Cache\ApcCache;
-        $config->setMetadataCacheImpl($cache);
-        $config->setQueryCacheImpl($cache);
-
-        // set up proxy configuration
-        $config->setProxyDir(APPPATH.'models/Proxies');
-        $config->setProxyNamespace('Proxies');
-        
-        // auto-generate proxy classes if we are in development mode
-        $config->setAutoGenerateProxyClasses(ENVIRONMENT == 'development');
-
-        // set up annotation driver
-        $yamlDriver = new \Doctrine\ORM\Mapping\Driver\YamlDriver(APPPATH.'models/Mappings');
-        $config->setMetadataDriverImpl($yamlDriver);
-
-        // Database connection information
-        $connectionOptions = array(
-            'driver' => 'pdo_mysql',
-            'user' => $db['default']['username'],
-            'password' => $db['default']['password'],
-            'host' => $db['default']['hostname'],
-            'dbname' => $db['default']['database']
+        $connection_options = array(
+            'driver'        => 'pdo_mysql',
+            'user'          => $db['default']['username'],
+            'password'      => $db['default']['password'],
+            'host'          => $db['default']['hostname'],
+            'dbname'        => $db['default']['database'],
+            'charset'       => $db['default']['char_set'],
+            'driverOptions' => array(
+                'charset' => $db['default']['char_set'],
+            ),
         );
-        
-        // create the EntityManager
-        $em = \Doctrine\ORM\EntityManager::create($connectionOptions, $config);
-        
-        // store it as a member, for use in our CodeIgniter controllers.
-        $this->em = $em;
+        $this->rsm          = new ResultSetMapping();
+        // With this configuration, your model files need to be in application/models/Entity
+        // e.g. Creating a new Entity\User loads the class from application/models/Entity/User.php
+        $models_namespace   = 'Entity';
+        $models_path        = APPPATH.'models';
+        $proxies_dir        = APPPATH.'models/Proxies';
+        $metadata_paths     = array(APPPATH.'models/Entity');
+
+        // Set $dev_mode to TRUE to disable caching while you develop
+        $dev_mode = true;
+
+        // If you want to use a different metadata driver, change createAnnotationMetadataConfiguration
+        // to createXMLMetadataConfiguration or createYAMLMetadataConfiguration.
+        $config = Setup::createAnnotationMetadataConfiguration($metadata_paths,
+                                                               $dev_mode,
+                                                               $proxies_dir);
+
+        $this->em = EntityManager::create($connection_options, $config);
+        $platform = $this->em->getConnection()->getDatabasePlatform();
+        $platform->registerDoctrineTypeMapping('enum', 'string');
+        $platform->registerDoctrineTypeMapping('set', 'string');
+        $loader   = new ClassLoader($models_namespace, $models_path);
+        $loader->register();
     }
 }
