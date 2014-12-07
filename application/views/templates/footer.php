8e5596c1708e61dd047e9a64564e3dea1fe09274
Brian-Webb <webbsdomain@gmail.com>
2014-12-07 07:33:23 -0700

    Added frontend files / template boilerplate

---
+++ application/views/templates/footer.php
@@ -0,0 +1,27 @@
+        </div>    
+        <!-- /main content -->
+
+        <!-- sidebar -->
+        <div class="medium-3 columns panel left">
+            <?php widget::run('character_list'); ?> 
+        </div>
+        <!-- /sidebar -->
+    
+    </div>
+    <!-- /content wrapper -->
+    
+    <!-- footer -->
+    <div class="row">
+        <div class="medium-12 columns panel">
+            <h1>Footer</h1>
+        </div>
+    </div>
+    <!-- /footer -->
+    
+    <script src="/js/vendor/jquery.js"></script>
+    <script src="/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php if(isset($page_title) && !empty($page_title)) {echo $page_title . ' | ';} ?>Sanguine Sunrise</title>
+    <link rel="stylesheet" href="/css/foundation.css" />
+    <script src="/js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    <!-- header -->
+    <div class="row">
+        <div class="medium-12 columns panel">
+            <h1>Header</h1>
+        </div>
+    </div>
+    <!-- /header -->
+    <!-- content wrapper -->
+    <div class="row">        
+        <!-- main content -->
+        <div class="medium-9 columns right panel callout">
