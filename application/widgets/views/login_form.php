eebc3f4a7fa1f778b41626dbbca5c1e5a9063b28
Brian-Webb <webbsdomain@gmail.com>
2014-12-25 01:28:45 -0700

    updating login form to work with new ajax versions

--- application/widgets/views/login_form.php
+++ application/widgets/views/login_form.php
@@ -1,9 +1,12 @@
+<!-- #login-form-wrapper -->
 <div id="login-form-wrapper">
-            <h3>Login</h3>
-        <form action="/user/login" method="POST" id="login-form">
-            <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash();?>" />
-            <input type="email" name="email" />
-            <input type="password" name="password" />
-            <input type="submit" value="Login" class="button" id="login-form-submit" />
-        </form>
-    </div>
+    <h3>Login</h3>
+    <div id="login-form-error"></div>
+    <form id="login-form">
+        <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" id="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash();?>" />
+        <input type="email" name="email" id="login-form-email" onkeydown="enterKeySubmit('login-form-submit',event);" />
+        <input type="password" name="password" id="login-form-password" onkeydown="enterKeySubmit('login-form-submit',event);" />
+        <a class="button" href="#" id="login-form-submit" onclick="loginSubmit();return false;">Login</a>
+    </form>
+</div>
+<!-- /#login-form-wrapper -->
